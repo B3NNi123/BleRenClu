56453ce221d8346c8bb14fe14abd7a1f1f093cb2
B3NNi123 <reinecke-domblatt@web.de>
2015-10-18 13:54:42 +0200

    Create db.php

---
+++ php-backend/client-side/login/db.php
@@ -0,0 +1,7 @@
+<?php
+define('DB_SERVER', 'nuntius.minted.de');
+define('DB_USERNAME', 'mysqluser');
+define('DB_PASSWORD', 'mintdatsheet21');
+define('DB_DATABASE', 'users');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
